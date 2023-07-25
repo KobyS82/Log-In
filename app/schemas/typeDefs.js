aaa0dd5f381cf5ca73c3743a96c016a73de3b707
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-07-25 18:27:41 -0500

    added models and schemas

---
+++ app/schemas/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+  }
+
+  # Set up an Auth type to handle returning data from a user creating or user login
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user: [User]!
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    # Set up mutations to handle creating a user or logging into a user and return Auth type
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    removeUser(userId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
